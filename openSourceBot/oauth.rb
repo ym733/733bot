c7e0de8ef841fd08cbb2d7839ba732c56d81b7d4
ym733 <70092518+ym733@users.noreply.github.com>
2022-06-29 01:50:29 +0300

    Update oauth.rb
    added comments

--- openSourceBot/oauth.rb
+++ openSourceBot/oauth.rb
@@ -1,6 +1,8 @@
-
+#get your oauth token and type it here
 $oauth = "oauth:xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 
+#type your twitch username here (the one that the bot will be running on)
 $username = "username"
 
+#type the channel you want the bot to join here
 $channel = "channel"
