feae4be1425626f4c6164840ff64beb409e5212f
ym733 <ym7330694@gmail.com>
2023-09-04 19:27:25 +0300

    added require 'json' in main.rb

--- openSource-IRC/main.rb
+++ openSource-IRC/main.rb
@@ -1,8 +1,11 @@
 #Program starts here
+
 system "cls"
 puts "Program running"
 
+require 'json'
 load 'irc.rb'
+
 $data = JSON.parse(File.read('./config.json'))
 
 irc = IRC.new($data["username"], $data["channel"], $data["oauth"])
